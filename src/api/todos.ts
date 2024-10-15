687c380e443cc44a9eeae383d8e9e802590310bb
Vasyliazhenko Dmytro Vitaliyovych <demon4225@gmail.com>
2024-10-15 17:55:18 +0300

    fixed UserId in api

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,8 +1,7 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-// Set your registered user ID here
-export const USER_ID = 123; // Replace with your actual user ID
+export const USER_ID = 1625;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
